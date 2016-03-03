f430ac145f7e50b08d6ab4627d060f4ec8a35e80
bablu258 <bablu452@yahoo.com>
2016-03-03 01:01:19 -0800

    This is first push

---
+++ Number/src/SimClass.java
@@ -0,0 +1,20 @@
+
+public class SimClass {
+
+	int price = 5000;
+	String color = "Black";
+	int year = 2011;
+	
+	
+	
+	public static void main(String[] args) {
+		
+		SimClass A = new SimClass ();
+		System.out.println (A.color);
+		System.out.println (A.price);
+		System.out.println (A.year);
+		
+
+	}
+
+}
---
+++ Number/src/SumOfNumber.java
@@ -0,0 +1,29 @@
+import javax.swing.JOptionPane;
+
+
+public class SumOfNumber {
+
+	public static void main(String[] args) {
+		
+		int num, sum=0, counter;
+		String str;
+		
+		do {
+			
+			str = JOptionPane.showInputDialog ( " enter a positive nonzero number: ");
+			
+			if (str == null)
+				System.exit(0);
+			
+			num= Integer.parseInt(str);
+			
+		} while(num <= 0);
+		
+		for (counter = 1; counter <= num; counter++)
+			sum = sum + counter;
+
+		JOptionPane.showMessageDialog ( null, "The total amount is : " + sum );
+
+	}
+
+}
